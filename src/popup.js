21e0929767342cbf04710dddf44ff6ff72cc7673
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-11-14 19:25:13 -0400

    refresh the page when adding an api key

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,13 @@
 let clipiaKey = document.getElementById('clipiaKey');
+const form = document.getElementById("form");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    chrome.storage.sync.set({clipiaKey: clipiaKey.value});
+    chrome.tabs.getSelected(null, tab => {
+        chrome.tabs.executeScript(tab.id, { code: "window.location.reload()" });
+    })
+})
 chrome.storage.sync.get(["clipiaKey"], item => {
     clipiaKey.value = item.clipiaKey;
 })
-clipiaKey.onchange = function(e) {
-    chrome.storage.sync.set({clipiaKey: e.target.value});
-};
