35d49d66643f082c34d9d42992bef55062aa8b0d
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-11-14 18:36:54 -0400

    stlylize popup

--- src/popup.js
+++ src/popup.js
@@ -3,7 +3,5 @@ chrome.storage.sync.get(["clipiaKey"], item => {
     clipiaKey.value = item.clipiaKey;
 })
 clipiaKey.onchange = function(e) {
-    chrome.storage.sync.set({clipiaKey: e.target.value}, () => {
-        console.log("Set clipia key to ", e.target.value);
-    });
+    chrome.storage.sync.set({clipiaKey: e.target.value});
 };
