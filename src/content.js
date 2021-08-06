9baba82ce8bd528df33b4e8669f80722fa6f9039
Joshua LeBlanc <joshleblanc94@gmail.com>
2021-08-06 20:06:01 -0300

    fix: properly parse RPC message

--- src/content.js
+++ src/content.js
@@ -1,29 +1,32 @@
 import MediaContainer from './components/media_container.svelte';
-import { when, observable } from 'mobx';
+import { when, observable, autorun } from 'mobx';
 
-let captures = observable({});
+let captures = observable.map({});
 
 // Options for the observer (which mutations to observe)
 const config = { attributes: true, childList: true, subtree: true };
 
 const callback = function (mutationsList, observer) {
+    
     mutationsList.forEach(mutation => {
         if (mutation.type === 'childList') {
             const containerNode = Array.from(mutation.addedNodes).find(node => {
                 return node && node.querySelector && node.querySelector(`[data-capture-id]`)
             })
             if (containerNode) {
+                
                 const icons = containerNode.querySelectorAll('.google-material-icons');
                 const lastIcon = icons[icons.length - 1];
                 const row = lastIcon.parentElement.parentElement.parentElement.parentElement.parentElement;
                 const videoNode = containerNode.querySelector(`[data-capture-id]`);
                 const buttons = row.children[2];
+                
                 const placeholder = document.createElement('div');
                 buttons.prepend(placeholder);
-                when(() => captures[videoNode.dataset.captureId], () => {
+                when(() => captures.has(videoNode.dataset.captureId), () => {
                     new MediaContainer({
                         target: placeholder,
-                        props: captures[videoNode.dataset.captureId]
+                        props: captures.get(videoNode.dataset.captureId)
                     });
                 })
             }
@@ -35,7 +38,7 @@ const callback = function (mutationsList, observer) {
 // the code that generates is injected directly into the page and 
 // can't set it itself
 document.addEventListener('update-captures', e => {
-    captures = e.detail;
+    captures.replace(e.detail);
 })
 
 const observer = new MutationObserver(callback);
@@ -48,23 +51,13 @@ const inject = () => {
     // https://github.com/refi64/stratos/blob/main/lib/inject/stadia_rpc_parser.dart#L19
     function decodeRpc(data) {
         let i = 0;
-        while (true) {
-            const responseStart = data.indexOf('[', i);
-            if (responseStart == -1) {
-                break;
-            }
-
-            const responseEnd = data.indexOf('\n]\n', responseStart);
-            if (responseEnd == -1) {
-                return [];
-            }
-
-            var response = JSON.parse(data.substring(responseStart, responseEnd + 2));
-            if (response[0][1] === rpcQueryId) {
-                return JSON.parse(response[0][2]);
-            }
-
-            i = responseEnd + 1;
+        const lines = data.split("\n");
+        const message = lines.filter(a => a.includes(rpcQueryId));
+        const outer = JSON.parse(message);
+        if (outer[0][1] === rpcQueryId) {
+            return JSON.parse(outer[0][2]);
+        } else {
+            return [];
         }
     }
 
@@ -73,20 +66,20 @@ const inject = () => {
         this.addEventListener('load', () => {
             const urlSearchParams = new URL(this.responseURL).searchParams;
             const idString = urlSearchParams.get('rpcids');
+
             if (idString && idString.includes(rpcQueryId)) {
                 const data = decodeRpc(this.response);
 
-                const captures = {};
+                const detail = {};
 
                 data[0].forEach(datum => {
                     const id = datum[1];
                     const game = datum[3];
                     const isImage = datum[8] === null;
                     const url = isImage ? datum[7][1] : datum[8][1];
-                    captures[id] = { id, game, isImage, url };
+                    detail[id] = { id, game, isImage, url };
                 });
-
-                const event = new CustomEvent("update-captures", { detail: captures });
+                const event = new CustomEvent("update-captures", { detail });
                 document.dispatchEvent(event);
             }
         });
